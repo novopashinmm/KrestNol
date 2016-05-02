5685b1116e15dbd431b6af38666f5ebe14961c00
Misha <novopashinmm@mail.ru>
2016-05-02 14:19:34 +0300

    Форматирование кода решарпером

--- Krestiki/Krestiki/KrestikiGame.cs
+++ Krestiki/Krestiki/KrestikiGame.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Krestiki.Properties;
 
 namespace Krestiki
 {
     public partial class KrestikiGame : Form
     {
-        private List<PictureBox> list;
+        private List<PictureBox> _list;
 
-        private int i = 0;
+        private int _i;
         public KrestikiGame()
         {
             InitializeComponent();
-            list = new List<PictureBox>();
+            _list = new List<PictureBox>();
         }
 
         private void Pole_MouseClick(object sender, MouseEventArgs e)
         {
             // Определяем что будем рисовать X или O
-            Bitmap bitmap;
-            bitmap = i%2 == 0 ? Krestiki.Properties.Resources.Krest : Krestiki.Properties.Resources.Nolik;
+            var bitmap = _i%2 == 0 ? Resources.Krest : Resources.Nolik;
 
             // Рисуем X или O
             CreateXorO(sender, e, bitmap);
 
             // Все нарисованные X или O вносим в список
-            list = (from object control in this.Controls where control.GetType().ToString().Contains("PictureBox") where ((PictureBox) control).Name.Contains("El") select control as PictureBox).ToList();
+            _list = (from object control in Controls where control.GetType().ToString().Contains("PictureBox") where ((PictureBox) control).Name.Contains("El") select control as PictureBox).ToList();
 
             // Флаг нужный для выхода из внешнего цикла
             bool flagBreak = false;
@@ -39,25 +35,24 @@ namespace Krestiki
             // В одной итерации проверяются крестики на выигрыш в другом нолики
             for (int n = 0; n < 2; n++)
             {
-                string elKorN;
-                elKorN = n == 0 ? "K" : "N";
-                foreach (var p1 in list.Where(p1 => p1.Name.Contains(elKorN)))
+                var elKorN = n == 0 ? "K" : "N";
+                foreach (var p1 in _list.Where(p1 => p1.Name.Contains(elKorN)))
                 {
-                    foreach (var p2 in list.Where(p2 => p2.Name.Contains(elKorN)))
+                    foreach (var p2 in _list.Where(p2 => p2.Name.Contains(elKorN)))
                     {
-                        foreach (var p3 in list.Where(p3 => p3.Name.Contains(elKorN)))
+                        foreach (var p3 in _list.Where(p3 => p3.Name.Contains(elKorN)))
                         {
-                            int p1x = p1.Location.X/80;
-                            int p2x = p2.Location.X/80;
-                            int p3x = p3.Location.X/80;
-                            int p1y = p1.Location.Y/80;
-                            int p2y = p2.Location.Y/80;
-                            int p3y = p3.Location.Y/80;
+                            int p1X = p1.Location.X/80;
+                            int p2X = p2.Location.X/80;
+                            int p3X = p3.Location.X/80;
+                            int p1Y = p1.Location.Y/80;
+                            int p2Y = p2.Location.Y/80;
+                            int p3Y = p3.Location.Y/80;
                             // Условие победы если подряд и они находятся на разных горизонталях
                             // x o o
                             // x o o
                             // x o o
-                            if (p1x == p2x && p2x == p3x && p1y != p2y && p1y != p2y && p2y != p3y && p1 != p2 &&
+                            if (p1X == p2X && p2X == p3X && p1Y != p2Y && p1Y != p2Y && p2Y != p3Y && p1 != p2 &&
                                 p1 != p3 && p2 != p3)
                             {
                                 CreateFinishLines(p1, p2, p3, "V");
@@ -69,7 +64,7 @@ namespace Krestiki
                             // x x x
                             // o o o
                             // o o o
-                            if (p1y == p2y && p2y == p3y && p1x != p2x && p1x != p2x && p2x != p3x && p1 != p2 &&
+                            if (p1Y == p2Y && p2Y == p3Y && p1X != p2X && p1X != p2X && p2X != p3X && p1 != p2 &&
                                 p1 != p3 && p2 != p3)
                             {
                                 CreateFinishLines(p1, p2, p3, "G");
@@ -82,11 +77,9 @@ namespace Krestiki
                             // o o x
                             // o x o
                             // x o o
-                            Point pWithMinX = new Point();
-                                pWithMinX = p1.Location.X < p2.Location.X && p1.Location.X < p3.Location.X ? p1.Location : p2.Location.X < p3.Location.X ? p2.Location : p3.Location;
-                            Point pWithMaxX = new Point();
-                                pWithMaxX = p1.Location.X > p2.Location.X && p1.Location.X > p3.Location.X ? p1.Location : p2.Location.X > p3.Location.X ? p2.Location : p3.Location;
-                                if (p1x != p2x && p1y != p2y && p1x != p3x && p1y != p3y && p2x != p3x && p2y != p3y && pWithMinX.Y / 80 != 1 && pWithMaxX.Y/80 != 1)
+                            var pWithMinX = p1.Location.X < p2.Location.X && p1.Location.X < p3.Location.X ? p1.Location : p2.Location.X < p3.Location.X ? p2.Location : p3.Location;
+                            var pWithMaxX = p1.Location.X > p2.Location.X && p1.Location.X > p3.Location.X ? p1.Location : p2.Location.X > p3.Location.X ? p2.Location : p3.Location;
+                                if (p1X != p2X && p1Y != p2Y && p1X != p3X && p1Y != p3Y && p2X != p3X && p2Y != p3Y && pWithMinX.Y / 80 != 1 && pWithMaxX.Y/80 != 1)
                             {
                                 CreateFinishLines(p1, p2, p3, "D");
                                 EndGame(sender, e, elKorN);
@@ -102,7 +95,7 @@ namespace Krestiki
                 }
             }
 
-            i++;
+            _i++;
         }
 
         public void EndGame(object sender, EventArgs args, string elKorN)
@@ -119,10 +112,11 @@ namespace Krestiki
         /// <param name="p1"></param>
         /// <param name="p2"></param>
         /// <param name="p3"></param>
-        private void CreateFinishLines(PictureBox p1, PictureBox p2, PictureBox p3, string FinishLine)
+        /// <param name="finishLine">Тип линии</param>
+        private void CreateFinishLines(PictureBox p1, PictureBox p2, PictureBox p3, string finishLine)
         {
             Point pWithMinX = new Point();
-            if (FinishLine == "D")
+            if (finishLine == "D")
                 pWithMinX = p1.Location.X < p2.Location.X && p1.Location.X < p3.Location.X ? p1.Location : p2.Location.X < p3.Location.X ? p2.Location : p3.Location;
 
             for (int j = 0; j < 3; j++)
@@ -132,7 +126,7 @@ namespace Krestiki
                     Name = "F" + j,
                     Width = 80,
                     Height = 80,
-                    BackgroundImage = FinishLine == "V" ? Krestiki.Properties.Resources.VertLine : FinishLine == "G" ? Krestiki.Properties.Resources.GorLine : pWithMinX.Y / 80 == 0 ? Krestiki.Properties.Resources.Diag1 : Krestiki.Properties.Resources.Diag2,
+                    BackgroundImage = finishLine == "V" ? Resources.VertLine : finishLine == "G" ? Resources.GorLine : pWithMinX.Y / 80 == 0 ? Resources.Diag1 : Resources.Diag2,
                 };
                 if (j == 0)
                     picFinish.Location = p1.Location;
@@ -140,7 +134,7 @@ namespace Krestiki
                     picFinish.Location = p2.Location;
                 if (j == 2)
                     picFinish.Location = p3.Location;
-                this.Controls.Add(picFinish);
+                Controls.Add(picFinish);
                 picFinish.Show();
                 picFinish.BringToFront();
             }
@@ -161,13 +155,13 @@ namespace Krestiki
             point.Y = point.Y + y*80;
             PictureBox pic = new PictureBox
             {
-                Name = "El" + i + (i%2 == 0 ? "K" : "N"),
+                Name = "El" + _i + (_i%2 == 0 ? "K" : "N"),
                 Width = 80,
                 Height = 80,
                 BackgroundImage = bitmap,
                 Location = point
             };
-            this.Controls.Add(pic);
+            Controls.Add(pic);
             pic.Show();
             pic.BringToFront();
         }
@@ -177,14 +171,14 @@ namespace Krestiki
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void новаяИграToolStripMenuItem_Click(object sender, System.EventArgs e)
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
         {
             List<PictureBox> allControlsForRemove = new List<PictureBox>();
-            allControlsForRemove.AddRange(list);
+            allControlsForRemove.AddRange(_list);
             allControlsForRemove.AddRange(FindLineControl());
             foreach (var control in allControlsForRemove)
             {
-                this.Controls.Remove(control);
+                Controls.Remove(control);
             }
         }
 
@@ -194,14 +188,13 @@ namespace Krestiki
         /// <returns></returns>
         private List<PictureBox> FindLineControl()
         {
-            List<PictureBox> listLines = new List<PictureBox>();
-            listLines = (from object control in this.Controls where control.GetType().ToString().Contains("PictureBox") where ((PictureBox)control).Name.Contains("F") select control as PictureBox).ToList();
+            var listLines = (from object control in Controls where control.GetType().ToString().Contains("PictureBox") where ((PictureBox)control).Name.Contains("F") select control as PictureBox).ToList();
             return listLines;
         }
 
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            base.Close();
+            Close();
         }
     }
 }
